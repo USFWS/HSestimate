d216d107f31961512e299ca339bebb30f6177b92
Abby Walter <abby_walter@fws.gov>
2026-05-27 11:07:43 -0400

    add pkg startup message; fixes #2

---
+++ R/variables.R
@@ -0,0 +1,14 @@
+# Define variables that change with each hunting season
+
+# Current hunting season (the year when the season opens)
+REF_CURRENT_SEASON <- "2025"
+
+# HSestimate package versions and their intended HIP season
+REF_RELEASES <-
+  c("dev" = "0.1.0",
+    "dev" = "0.2.0",
+    "dev" = "0.3.0",
+    "dev" = "1.0.0",
+    "dev" = "1.0.1",
+    `2025-2026` = "2.0.0"
+  )
---
+++ R/zzz.R
@@ -0,0 +1,58 @@
+#' @importFrom utils packageVersion
+
+.onAttach <- function(libname, pkgname = "HSestimate") {
+  
+  local_version <- packageVersion(pkgname)
+  season <- names(REF_RELEASES[REF_RELEASES == local_version])
+  
+  try({
+    desc_url <-
+      paste0(
+        "https://raw.githubusercontent.com/USFWS/HSestimate/refs/heads/main/",
+        "DESCRIPTION")
+    remote_desc <- readLines(desc_url, warn = FALSE)
+    remote_version <-
+      gsub("Version: ", "", remote_desc[grep("^Version: ", remote_desc)])
+    
+    # If the local package version is not in the REF_RELEASES internal object,
+    # return a message.
+    if (!local_version %in% REF_RELEASES) {
+      packageStartupMessage(
+        "Local package version does not exist in REF_RELEASES.")
+      # If the local package version is considered a "development" version,
+      # notify the user that the version was not fully finalized.
+    } else if (season == "dev" & local_version %in% REF_RELEASES) {
+      packageStartupMessage(
+        "HSestimate",
+        " v",
+        local_version,
+        " is an incomplete package version.",
+        "\nThis version is NOT fully finalized for any season of HS data!"
+      )
+      # If the local version is older than the most up-to-date release on
+      # GitHub, tell the user to update the package.
+    } else if (local_version < remote_version) {
+      packageStartupMessage(
+        "A newer version of HSestimate (v",
+        remote_version,
+        ") is available.",
+        "\nYou are currently using v",
+        local_version,
+        ", which was released for the ",
+        season,
+        " season.\nUpdate with pak::pak('USFWS/HSestimate')"
+      )
+      # If the local version is equivalent to the most up-to-date release on
+      # GitHub, tell the user that it's current.
+    } else if (local_version == remote_version &
+               season != "dev" &
+               local_version %in% REF_RELEASES) {
+      packageStartupMessage("HSestimate",
+                            " v",
+                            local_version,
+                            "\nCompatible with ",
+                            season,
+                            " Harvest Survey data")
+    }
+  }, silent = TRUE)
+}
